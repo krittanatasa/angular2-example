b9700ed9bbae23c02c414096f5b1f9281e00aefc
krittanatasa <krittanatasa@gosoft.co.th>
2017-03-10 11:10:05 +0700

    Hero Class Example

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gosoft-root',
-  template: '<h1>GOSOFT</h1> <h1>{{title}}</h1> <h2>{{titie_2}}</h2>'
+  template: `<h1>GOSOFT</h1> <h1>{{title}}</h1>
+            <h2>{{hero.title}}</h2>`
 })
 export class GosoftComponent {
-  title = 'Gosoft Component!';
-  // titie_2 = 'test';
+    title = 'Gosoft Component!';
+    //titie_2 = 'test';
+    hero: Hero = new Hero(1, 'Iron Man')
+  //  hero_2: Hero = {id: 2, title: 'Super man'}
+}
+
+class Hero {
+  constructor(private id: number, private title: string) {}
 }
