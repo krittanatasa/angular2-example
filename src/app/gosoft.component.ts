f50b0a79e5c214ae97fe75c1baed84610b88ef4b
krittanatasa <krittanatasa@gosoft.co.th>
2017-03-10 15:03:38 +0700

    get hero array from HeroService

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './mock.heroes';
+//import { HEROES } from './mock.heroes';
+import { HeroService } from './hero.service';
+
 
 @Component({
   selector: 'gosoft-root',
   templateUrl: './gosoft.component.html',
-  styleUrls : ['gosoft.component.css']
+  styleUrls : ['gosoft.component.css'],
+  providers : [HeroService]
 })
 export class GosoftComponent {
-  
+    
+    heroes: Hero[];
     title = 'Gosoft Component!';
     selectedHero: Hero;
-
-    heroes = HEROES;
+    constructor(private heroService: HeroService) {
+        this.heroes = this.heroService.getHero();
+    }
+    
   //  hero_2: Hero = {id: 2, title: 'Super man'},
     doSomething(hero): void {
       console.log("click " + hero.name);
