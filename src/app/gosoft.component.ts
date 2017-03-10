9dd2554041575636903dd7840e941b5bbad31f59
krittanatasa <krittanatasa@gosoft.co.th>
2017-03-10 10:32:08 +0700

    modify template

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'gosoft-root',
-  template: '<h1>GOSOFT</h1> <h1>{{title}}</h1> <h2>{{titie_2}}</h2>'
+  template: '<h1>GOSOFT</h1> <h1>{{title}}</h1>'
 })
 export class GosoftComponent {
   title = 'Gosoft Component!';
-  // titie_2 = 'test';
 }
