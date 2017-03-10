0682f3e9f6c4e1f3a02e7c4d0abfc1e38ab3d858
krittanatasa <krittanatasa@gosoft.co.th>
2017-03-10 15:07:43 +0700

    use OnInit to inittial

--- src/app/gosoft.component.ts
+++ src/app/gosoft.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
-//import { HEROES } from './mock.heroes';
 import { HeroService } from './hero.service';
 
 
 @Component({
   selector: 'gosoft-root',
   templateUrl: './gosoft.component.html',
-  styleUrls : ['gosoft.component.css'],
-  providers : [HeroService]
+  styleUrls : ['gosoft.component.css']
 })
-export class GosoftComponent {
+export class GosoftComponent implements OnInit {
     
     heroes: Hero[];
     title = 'Gosoft Component!';
     selectedHero: Hero;
-    constructor(private heroService: HeroService) {
+    heroService: HeroService;
+ 
+    ngOnInit(): void {
+        this.heroService = new HeroService();
         this.heroes = this.heroService.getHero();
     }
-    
-  //  hero_2: Hero = {id: 2, title: 'Super man'},
+
     doSomething(hero): void {
       console.log("click " + hero.name);
       this.selectedHero = hero;
