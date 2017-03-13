e897f6f518d8de138156bd7eccf9ae1de385c4c1
krittanatasa <krittanatasa@gosoft.co.th>
2017-03-13 22:41:00 +0700

    create app routing module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GosoftComponent } from './gosoft.component';
+import { GosoftDetail } from './gosoft.detail';
+import { DashBoard } from './dashboard.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashBoard },
+  { path: 'detail/:id', component: GosoftDetail },
+  { path: 'heroes',     component: GosoftComponent }
+];
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
